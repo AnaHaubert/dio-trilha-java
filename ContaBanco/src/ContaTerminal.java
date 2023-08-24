e8be0f0445274d502ee5c322b467e34e1f7e0a98
Ana Haubert <anachaubertf@gmail.com>
2023-08-24 16:42:12 +0100

    projeto conta branco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //dados do cliente
+        System.out.println("Digite o nome do cliente");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite a agência.");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da conta.");
+        int numeroConta = scanner.nextInt();
+
+        //extrato
+        System.out.println("Digite o valor do depósito inicial.");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + "! Obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", sua conta é " + numeroConta + " e seu saldo no valor de R$ " + String.format("%.2f", saldo) + " já está disponível para saque!");
     }
 }
